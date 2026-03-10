7335c2183027da37f3d58e5ed41a490602a7c147
zeads <ruri.pelinandang@gmail.com>
2026-03-10 07:51:28 +0700

    feat: test page number

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,8 @@
 <x-layout :title="$title">
         <p>Welcome to my home page.</p>
+        <div class="flex gap-2">
+            @for ($i=1; $i<=10; $i++)
+                <div class="w-8 h-8 bg-teal-500 text-white text-xs p-0 me-1 grid place-items-center">{{ $i }}</div>
+            @endfor
+        </div>
 </x-layout>
