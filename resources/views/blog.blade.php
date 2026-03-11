3ff102eda29ba6581c4b923b7d8ef66bcfb353d2
zeads <ruri.pelinandang@gmail.com>
2026-03-11 15:22:26 +0700

    feat: add blog dummy data

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,3 +1,13 @@
 <x-layout :title="$title">
-    <p>This is my blog page.</p>
+    @foreach ($posts as $post )
+        <article class="py-1 max-w-3xl border-b border-gray-200">
+            <h1 class="mb-1 text-2xl tracking-tight ">{{ $post['title'] }}</h1>
+            <div class="mb-4 text-sm text-gray-400">
+                <a href="#"> {{ $post['author'] }} | 1 Januari 2023</a>
+            </div>
+            <p class="mb-4 leading-relaxed text-gray-500">
+                {{ Str::limit($post['body'],100) }}
+            </p>
+        </article>
+    @endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,25 @@ Route::get('/', function () {
 });
 
 Route::get('/blog', function () {
+    $posts = [
+        [
+            'title' => 'Judul Artikel Pertama',
+            'author' => 'Ruri Pelinandang',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Magnam molestias unde libero velit cumque eligendi tenetur natus accusamus totam,
+            laborum quibusdam ad? Tempora, iusto expedita quis laborum necessitatibus iure quidem?'
+        ],
+        [
+            'title' => 'Judul Artikel kedua',
+            'author' => 'Ruri Pelinandang',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Quae Eveniet delectus iste ullam, ut ducimus recusandae culpa libero.
+            Nihil odio quidem, beatae unde ratione totam eligendi animi delectus autem dolorum tempore?'
+        ]
+    ];
     return view('blog', [
-        'title' => 'Blog'
+        'title' => 'Blog',
+        'posts' => $posts
     ]);
 });
 
