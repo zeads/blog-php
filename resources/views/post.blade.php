f0ced6ee58c35555740633a7aa031c129765f043
zeads <ruri.pelinandang@gmail.com>
2026-03-12 09:58:19 +0700

    feat: add single post by id

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,16 @@
 <x-layout :title="$title">
-    @foreach ($posts as $post )
+
         <article class="py-4 max-w-3xl border-b border-gray-200">
             <h1 class="mb-1 text-2xl font-bold tracking-tight ">{{ $post['title'] }}</h1>
             <div class="mb-4 text-sm text-gray-400">
                 <a href="#"> {{ $post['author'] }} | 1 Januari 2023</a>
             </div>
             <p class="mb-4 leading-relaxed text-gray-500">
-                {{ Str::limit($post['body'],100) }}
+                {{ $post['body'] }}
             </p>
-            <a href="/posts/{{ $post['id'] }}" class="text-blue-600 hover:underline">Read more &raquo;</a>
+
+            <a href="/posts" class="text-blue-600 hover:underline">&laquo; Back to all posts</a>
+
         </article>
-    @endforeach
+
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,9 +9,10 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/blog', function () {
+Route::get('/posts', function () {
     $posts = [
         [
+            'id' => 1,
             'title' => 'Judul Artikel Pertama',
             'author' => 'Ruri Pelinandang',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -18,6 +20,7 @@ Route::get('/blog', function () {
             laborum quibusdam ad? Tempora, iusto expedita quis laborum necessitatibus iure quidem?'
         ],
         [
+            'id' => 2,
             'title' => 'Judul Artikel kedua',
             'author' => 'Ruri Pelinandang',
             'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -30,6 +33,39 @@ Route::get('/blog', function () {
         'posts' => $posts
     ]);
 });
+
+Route::get('posts/{id}', function ($id) {
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'Judul Artikel Pertama',
+            'author' => 'Ruri Pelinandang',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Magnam molestias unde libero velit cumque eligendi tenetur natus accusamus totam,
+            laborum quibusdam ad? Tempora, iusto expedita quis laborum necessitatibus iure quidem?'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel kedua',
+            'author' => 'Ruri Pelinandang',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Quae Eveniet delectus iste ullam, ut ducimus recusandae culpa libero.
+            Nihil odio quidem, beatae unde ratione totam eligendi animi delectus autem dolorum tempore?'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($id) {
+        return $post['id'] == $id;
+    });
+
+    // dd($post);
+    return view('post', [
+        'title' => 'Single Post',
+        'post' => $post
+    ]);
+});
+
+
 
 Route::get('/about', function () {
     return view('about', [
